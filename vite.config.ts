62ebee8e7504812b98ff03a4a46a92af74e02526
WakhidHasim <wakhidhasim2000@gmail.com>
2024-03-02 09:57:31 +0700

    change proxy url

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://103.86.100.20:3001/api',
-        target: 'http://localhost:3000/api',
+        target: 'http://103.86.100.20:3001/api',
+        // target: 'http://localhost:3000/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
